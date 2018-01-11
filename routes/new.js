d88f6d06e95436fa74b6b03eed3b0d69ea68e3d2
Glitch (rogue-brow) <none>
2018-01-11 12:29:39 +0000

    🐢🐬 Checkpoint server.js:577470/285 routes/new.js:577470/623

---
+++ routes/new.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('./:url(*)', (req, res, next) => {
+  const url = req.params.url
+  
+  res.json({
+    original: url
+  })
+}) 
+
+module.exports = router
