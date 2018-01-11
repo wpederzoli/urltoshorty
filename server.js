582be68a096891f60d607d18fabd1db9a1807f55
Glitch (rogue-brow) <none>
2018-01-11 12:40:11 +0000

    👄💼 Checkpoint server.js:577470/252

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express  = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 
 const newUrl = require('./routes/new')
 
-app.listen(process.env.PORT)
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.use('/new', newUrl)
+
+app.listen(process.env.PORT, () =>{
+  console.log('listening')
+})
                         
+
+module.exports = app
