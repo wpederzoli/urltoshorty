1354e6844806f3f5aa9f8ed98eb2acdb0a31197c
Glitch (rogue-brow) <none>
2018-01-12 11:02:00 +0000

    😰🖇 Checkpoint server.js:577470/116 routes/redirect.js:577470/1

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -13,11 +14,13 @@ const redirect = require('./routes/redirect')
 mongoose.Promise = require('bluebird')
 mongoose.connect('mongodb://anonymus:urlmicro@ds251747.mlab.com:51747/shorties')
 app.use(morgan('dev'))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/new', newUrl)
+app.use('/', redirect)
 
 app.listen(process.env.PORT)
                         
